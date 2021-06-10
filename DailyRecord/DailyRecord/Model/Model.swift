7062d214f9dbf0b986974995bac074c737f1844b
kimdoin <kimdi1995@naver.com>
2021-06-11 00:03:00 +0900

    테이블뷰 생성

---
+++ DailyRecord/DailyRecord/Model/Model.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "ff"),
+        Memo(content: "fg")
+    ]
+}
+
+
